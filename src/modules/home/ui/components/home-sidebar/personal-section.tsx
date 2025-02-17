9e3eeabcf7b3da05fbbf5dd74f1c7e258cbb4702
Gigher <gastelo1301@gmail.com>
2025-02-17 20:18:50 +0300

    chapter 3 | add clerk authentication through project

--- src/modules/home/ui/components/home-sidebar/personal-section.tsx
+++ src/modules/home/ui/components/home-sidebar/personal-section.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import { FlameIcon, HistoryIcon, HomeIcon, ListVideoIcon, PlaySquareIcon, ThumbsUpIcon } from "lucide-react";
+import Link from "next/link";
+import { useAuth, useClerk } from "@clerk/nextjs";
+import { HistoryIcon, ListVideoIcon, ThumbsUpIcon } from "lucide-react";
 
 import {
   SidebarGroup,
@@ -10,7 +12,6 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
-import Link from "next/link";
 
 const items = [
   {
@@ -34,11 +35,12 @@ const items = [
 ];
 
 export const PersonalSection = () => {
+  const clerk = useClerk();
+  const { isSignedIn } = useAuth();
+
   return (
     <SidebarGroup>
-      <SidebarGroupLabel>
-        You
-      </SidebarGroupLabel>
+      <SidebarGroupLabel>You</SidebarGroupLabel>
       <SidebarGroupContent>
         <SidebarMenu>
           {items.map((item) => (
@@ -47,7 +49,12 @@ export const PersonalSection = () => {
                 tooltip={item.title}
                 asChild
                 isActive={false} // TODO: Change to look at current pathname
-                onClick={() => {}} // TODO: Do something on click
+                onClick={(e) => {
+                  if (!isSignedIn && item.auth) {
+                    e.preventDefault();
+                    return clerk.openSignIn();
+                  }
+                }}
               >
                 <Link href={item.url} className="flex items-center gap-4">
                   <item.icon />
