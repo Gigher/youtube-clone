b96cbc7c20ee35a5f871ff9c6d13984941cb6e4f
Gigher <gastelo1301@gmail.com>
2025-03-12 15:43:18 +0300

    chapter 6 and 7 | add tRPC and configure it

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,20 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
-export default function Home() {
+import { HydrateClient, trpc } from "@/trpc/server";
+
+import { PageClient } from "./client";
+
+export default async function Home() {
+  void trpc.hello.prefetch({ text: "Ilya" });
+
   return (
-    <div>
-      I will load videos in the future!
-    </div>
+    <HydrateClient>
+      <Suspense fallback={<p>Loading...</p>}>
+        <ErrorBoundary fallback={<p>Error...</p>}>
+          <PageClient />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrateClient>
   );
 }
